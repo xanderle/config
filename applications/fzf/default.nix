fc8561c3cecec53d57fe6dc9ea42b6f5f631d722
xanderle <alex@xanderle.xyz>
2020-12-27 09:37:31 +0000

    setopt vi first otherwise fzf fails

--- applications/fzf/default.nix
+++ applications/fzf/default.nix
@@ -4,5 +4,22 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    defaultCommand = ''rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null'';
+    defaultOptions = [
+  "--ansi"
+  "--layout=default"
+  "--info=inline"
+  "--height=50%"
+  "--multi"
+  "--preview-window=right:50%"
+  "--preview-window=sharp"
+  "--preview-window=cycle"
+  "--prompt='λ -> '"
+  "--pointer='|>'"
+  "--marker='✓'"
+  "--bind 'ctrl-e:execute(nvim {} < /dev/tty > /dev/tty 2>&1)' > selected"
+  "--bind 'ctrl-v:execute(code {+})'"
+];
+fileWidgetCommand = ''rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null'';
   };
 }
--- applications/zsh/default.nix
+++ applications/zsh/default.nix
@@ -2,46 +2,22 @@
 
 
 {
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = true;
   programs.zsh = {
     enable = true;
     autocd = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    initExtra = ''
-      setopt vi
-      export FZF_DEFAULT_OPTS="
-      --ansi
-      --layout=default
-      --info=inline
-      --height=50%
-      --multi
-      --preview-window=right:50%
-      --preview-window=sharp
-      --preview-window=cycle
-      --preview '([[ -f {} ]] && (bat --style=numbers --color=always --theme=gruvbox --line-range :500 {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
-      --prompt='λ -> '
-      --pointer='|>'
-      --marker='✓'
-      --bind 'ctrl-e:execute(nvim {} < /dev/tty > /dev/tty 2>&1)' > selected
-      --bind 'ctrl-v:execute(code {+})'"
-      export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
-      export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-      zstyle ':completion:*' completer _expand _complete _ignored _approximate
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-      zstyle ':completion:*' menu select=2
-      zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-      zstyle ':completion:*:descriptions' format '-- %d --'
-      zstyle ':completion:*:processes' command 'ps -au$USER'
-      zstyle ':completion:complete:*:options' sort false
-      zstyle ':fzf-tab:complete:_zlua:*' query-string input
-      zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd -w -w"
-      zstyle ':fzf-tab:complete:kill:argument-rest' extra-opts --preview=$extract'ps --pid=$in[(w)1] -o cmd --no-headers -w -w' --preview-window=down:3:wrap
-      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
-
-      '';
     plugins = [
+      {
+        name = "zsh-history-substring-search";
+        file = "zsh-history-substring-search.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "0f80b8eb3368b46e5e573c1d91ae69eb095db3fb";
+          sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
+        };
+      }
       {
          name = "fzf-tab";
           file = "fzf-tab.plugin.zsh";
@@ -63,6 +39,15 @@
         };
       }
     ];
+    initExtraFirst = ''
+      setopt vi
+      '';
+    initExtra = ''
+      bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+      '';
   };
   programs.starship = {
     enable = true;
